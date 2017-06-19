989b06aa2a688ae94c773bb2d2951d7950efaecd
Guarajuba <gcwarken@gmail.com>
2017-06-19 00:02:04 -0300

    created Group.java getter/setter

--- courseAPI/courseAPI/domain/Group.java
+++ courseAPI/courseAPI/domain/Group.java
@@ -1,5 +1,31 @@
 package courseAPI.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Group {
 
+	private int groupId;
+	private Teacher groupTeacher;
+	private int numStudents;
+	private List<Session> groupSessions;
+	
+	public Group(int id, Teacher teacher, int students) {
+		groupId = id;
+		groupTeacher = teacher;
+		numStudents = students;
+		groupSessions = new ArrayList<>();
+	}
+	
+	public int getGroupId() {
+		return groupId;
+	}
+
+	public Teacher getGroupTeacher() {
+		return groupTeacher;
+	}
+	
+	public int getNumStudents() {
+		return numStudents;
+	}
 }
