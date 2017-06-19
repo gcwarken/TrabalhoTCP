755871b2b9da5ca4ee1439d98606128b281f0878
Guarajuba <gcwarken@gmail.com>
2017-06-19 01:08:25 -0300

    created Feature.java getter/setter

--- courseAPI/courseAPI/domain/Feature.java
+++ courseAPI/courseAPI/domain/Feature.java
@@ -1,5 +1,26 @@
 package courseAPI.domain;
 
 public class Feature {
-
+	private int id;
+	private String name;
+	
+	public Feature(int _id, String _name) {
+		this.id = _id;
+		this.name = _name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
 }
+
+/*
+feature:
+	name
+	id
+	hidden (true/false)
+*/
