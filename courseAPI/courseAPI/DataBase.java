a6ead3475cd066b37c76c175be98fdb547a09e99
Guarajuba <gcwarken@gmail.com>
2017-06-19 15:24:52 -0300

    New DataBase.java class

---
+++ courseAPI/courseAPI/DataBase.java
@@ -0,0 +1,28 @@
+package courseAPI;
+
+import java.util.ArrayList;
+import java.util.List;
+import courseAPI.domain.*;
+
+public class DataBase {
+
+	private List<Teacher> teachers;
+	private List<Building> buildings;
+	private List<Course> courses;
+	private List<Feature> features;
+	private List<Group> groups;
+	private List<Room> rooms;
+	private List<Session> sessions;
+	
+	public DataBase() {
+		this.teachers = new ArrayList<>();
+		this.buildings = new ArrayList<>();
+		this.courses = new ArrayList<>();
+		this.features = new ArrayList<>();
+		this.groups = new ArrayList<>();
+		this.rooms = new ArrayList<>();
+		this.sessions = new ArrayList<>();
+	}
+	
+	
+}
--- courseAPI/courseAPI/fileHelper/XmlManipulation.java
+++ courseAPI/courseAPI/fileHelper/XmlManipulation.java
@@ -1,5 +1,17 @@
 package courseAPI.fileHelper;
 
-public class XmlManipulation {
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
 
+public  abstract class XmlManipulation {
+
+	private String inputXmlPath;
+	private String outputXmlPath;
+	
+	
 }
