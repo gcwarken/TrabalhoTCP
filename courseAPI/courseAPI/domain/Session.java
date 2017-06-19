a90b5a34b7d6f29436e0a2c457c6fd8e296e13c1
Guarajuba <gcwarken@gmail.com>
2017-06-19 01:05:08 -0300

    created Session.java getter/setter

--- courseAPI/courseAPI/domain/Session.java
+++ courseAPI/courseAPI/domain/Session.java
@@ -2,4 +2,44 @@ package courseAPI.domain;
 
 public class Session {
 
+	private Room sessionRoom;
+	private int sessionDuration;
+	private int	startTime;
+	private int weekday;
+	
+	public Session(Room room, int duration, int time, int day) {
+		this.sessionRoom = room;
+		this.sessionDuration = duration;
+		this.startTime = time;
+		this.weekday = day;
+	}
+	
+	public Room getSessionRoom() {
+		return sessionRoom;
+	}
+	
+	public int getSessionDuration() {
+		return sessionDuration;
+	}
+	
+	public int getStartTime() {
+		return startTime;
+	}
+	
+	public int getWeekday() {
+		return weekday;
+	}
+	
 }
+
+/*
+session:
+    room_id					* dá pra pegar do room
+    duration				* ok
+	building_id				* dá pra pegar do room
+    weekday					* ok
+    start_time				* ok
+    requires_room_id		?
+    requires_building_id	?
+    feature_ids				?
+*/
