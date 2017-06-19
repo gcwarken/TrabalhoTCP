fe70c8018c71befce0bedf7cb6adcd464adee4e8
Guarajuba <gcwarken@gmail.com>
2017-06-19 01:22:19 -0300

    created Room.java getter/setter

--- courseAPI/courseAPI/domain/Room.java
+++ courseAPI/courseAPI/domain/Room.java
@@ -1,5 +1,44 @@
 package courseAPI.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Room {
 
+	private int id;
+	private List<Feature> features;
+	private int capacity;
+	private boolean available = true;
+	
+	public Room(int _id, int _capacity) {
+		this.id = _id;
+		this.features = new ArrayList<>();
+		this.capacity = _capacity;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void roomNotAvailable() {
+		available = false;
+	}
+	
+	public boolean getAvailability() {
+		return available;
+	}
+	
+	
 }
+
+/*
+room:
+	id
+	feature_ids
+	number_of_places
+	available_for_allocation (true/false)
+*/
