bc84354acb372bb32d49a81e6accd0e11329ad7f
Guarajuba <gcwarken@gmail.com>
2017-06-19 01:13:37 -0300

    created Building.java getter/setter

--- courseAPI/courseAPI/domain/Building.java
+++ courseAPI/courseAPI/domain/Building.java
@@ -1,5 +1,30 @@
 package courseAPI.domain;
 
-public class Building {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Building {
+	
+	private int id;
+	private List<Room> rooms;
+	
+	public Building(int _id) {
+		this.id = _id;
+		this.rooms = new ArrayList<>();
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
 }
+
+/*
+building:
+	id
+	rooms:
+		id
+		feature_ids
+		number_of_places
+		available_for_allocation (true/false)
+*/        
