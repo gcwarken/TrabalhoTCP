dd70db4a705f4828cb543851eb8399550a151075
Guarajuba <gcwarken@gmail.com>
2017-06-30 17:45:05 -0300

    added xml files

--- courseAPI/courseAPI/CourseAPI.java
+++ courseAPI/courseAPI/CourseAPI.java
@@ -1,11 +1,15 @@
 package courseAPI;
 
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
 import courseAPI.api.*; 
 
 public class CourseAPI {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws ParserConfigurationException {
+		
+		Leitor l = new Leitor("test.xml");
+		Document dom = l.getFileInfo();
 	}
 
 }
