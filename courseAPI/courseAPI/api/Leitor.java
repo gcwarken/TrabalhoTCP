f6bfda9c231e957d61b15b2a9c4e44fb8bf42ec5
Guarajuba <gcwarken@gmail.com>
2017-06-30 16:34:38 -0300

    New methods for Leitor.java
    - public Document getFileInfo(String fileName)
    - private void readXml(String xml)

--- courseAPI/courseAPI/api/Leitor.java
+++ courseAPI/courseAPI/api/Leitor.java
@@ -3,19 +3,35 @@ package courseAPI.api;
 import java.util.ArrayList;
 import java.util.List;
 import courseAPI.DataBase;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
 import java.io.File;
 
 
 public class Leitor {
 	
-	private String inputXmlFilePath;
-	private DataBase apiDB;
-	
-	public void openFile() {
+	private String inputFilePath;
+	//private DataBase apiDB;
 		
+	public Document getFileInfo(String fileName) {
+		Document doc = null;
+		String extension = fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length());
+		
+		if (extension.equals("xml")) {
+			readXml(fileName);
+		}
+		
+		return doc;
+	}
+	
+	private void openFile() {
 	} 
 	
-	private String getXmlInfo() {
+	private void readXml(String xml) {
 		
 	}
 	
