09a704acc5619db9d182b9117ff93472c6c5d9a8
Guarajuba <gcwarken@gmail.com>
2017-06-30 18:46:39 -0300

    Created Tag*.java classes

--- courseAPI/courseAPI/api/Tag.java
+++ courseAPI/courseAPI/api/Tag.java
@@ -3,9 +3,9 @@ package courseAPI.api;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.w3c.dom.Document;
+
 
 public interface Tag {
-	public void createObject(List l);
-		
-	public String createXml (List l); 
+	public void fillDataBase(Document d);
 }
